c631df440e39bff3ad69719c105a96747885f741
tutss <artur_santos@usp.br>
2018-05-01 14:55:17 -0300

    adding nearest

--- 3sm-2018/doing/ep4-work/src/PointST.java
+++ 3sm-2018/doing/ep4-work/src/PointST.java
@@ -5,7 +5,6 @@
 
 
 import edu.princeton.cs.algs4.*;
-import edu.princeton.cs.algs4.Point2D;
 
 public class PointST<Value>{
     // Instance variables
@@ -13,7 +12,6 @@ public class PointST<Value>{
     // Public methods
     public PointST(){
         tree = new RedBlackBST<Point2D, Value>();
-
     }
 
     public boolean isEmpty() {
@@ -49,7 +47,18 @@ public class PointST<Value>{
     }
     public Iterable<Point2D> nearest(Point2D p, int k){
         if (p == null) throw new IllegalArgumentException();
-        return null;
+        Point2D nearest = null;
+        Stack<Point2D> s = new Stack<Point2D>();
+        for (Point2D point : tree.keys()) {
+            if (nearest == null || point.distanceTo(p) < nearest.distanceTo(p))
+                nearest = point;
+                s.push(nearest);
+        }
+        Stack<Point2D> ret = new Stack<Point2D>();
+        for (int i = 0; i < k; i++) {
+            ret.push(s.pop());
+        }
+        return ret;
     }
     // Private methods
 
