49314cb9f2a0eccd59bf7d41ecae653248cff933
tutss <artur_santos@usp.br>
2018-05-01 21:10:34 -0300

    making changes

--- 3sm-2018/doing/ep4-work/src/KdTreeST.java
+++ 3sm-2018/doing/ep4-work/src/KdTreeST.java
@@ -3,7 +3,7 @@
  *  Dependencies:
  */
 
-import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.*;
 
 public class KdTreeST{
 
@@ -14,8 +14,10 @@ public class KdTreeST{
 
         private Point2D p;
         private RectHV  rect;
+        private Value val;
         private Node    lf;
         private Node    rt;
+        private int level;
 
         public Node(Point2D p, RectHV rect) {
             RectHV r = rect;
@@ -23,6 +25,25 @@ public class KdTreeST{
                 r = new RectHV(0, 0, 1, 1);
             this.rect   = r;
             this.p      = p;
+            this.level = 0;
+        }
+
+        public int compare(Point2D point) {
+            if (level % 2 == 0) {
+                if (point.x() < p.x())
+                    return 1;
+                else if (point.x() > p.x())
+                    return -1;
+                else
+                    return 0;
+            } else {
+                if (point.y() < p.y())
+                    return 1;
+                else if (point.y() > p.y())
+                    return -1;
+                else
+                    return 0;
+            }
         }
     }
 
@@ -41,10 +62,36 @@ public class KdTreeST{
     }
 
     public boolean contains(Point2D p) {
-        return contains(root, p, true);
+        return get(p) != null;
     }
 
+    public void put(Point2D p, Value val) {
+        put(p, val, root);
+    }
+    private void put(Point2D p, Value val, Node node) {
+        if (node == null) ret
+    }
+    public Value get(Point2D p) {
+        return get(p, root, 0);
+    }
+
+    private Value get(Point2D p, Node node, int height) {
+        if (node == null) return null;
+        int comp = node.compare(p);
+        if (comp < 0) return get(p, node.lf, node.level + 1);
+        else if (comp > 0) return get(p, node.rt, node.level + 1);
+        else return node.val;
+    }
+    public Iterable<Point2D> points() {
+        return tree.keys();
+    }
+    public Iterable<Point2D> range(RectHV rect){
+
+    }
+    public Iterable<Point2D> nearest(Point2D p, int k){ }
+
     // Private methods
+
     private boolean contains(Node node, Point2D p, boolean b) {
         if (node == null) return false;
         if (node.p.equals(p)) return true;
